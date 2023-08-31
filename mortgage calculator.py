50dabd5abe9069dc4f9099dd3b79e83f21aaba17
inclinegod <143661601+inclinegod@users.noreply.github.com>
2023-09-01 00:27:56 +0100

    CMDLine Calculator to calculate mortgage cost
    calculates mortage cost over a set period of years based on a base interest rate + salary + bank interest rate

---
+++ mortgage calculator.py
@@ -0,0 +1,25 @@
+#mortgage calculator
+
+def borrowing():
+    while True:
+        try:
+            salary = int(input("input your salary "))
+        except ValueError as e:
+            e = print("wrong data type inputted ")
+            continue
+
+max_allowed = borrowing()*4
+
+print("the most you can borrow is,", max_allowed , "GBP")
+
+def timetopay():
+    while True:
+        try:
+            ttp = int(input("input years you would like to take a mortgage out for "))
+        except ValueError as e:
+            e = print("wrong data type inputted ")
+            continue
+        
+
+
+
