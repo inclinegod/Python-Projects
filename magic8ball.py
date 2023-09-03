1cfde803415711d9d4ccbb58230eeb601e14f8d7
inclinegod <143661601+inclinegod@users.noreply.github.com>
2023-09-03 02:01:59 +0100

    Update magic8ball.py
    Added #comments

--- magic8ball.py
+++ magic8ball.py
@@ -1,13 +1,19 @@
 import random
 
+#initalization + random num gen
 name = ("Alex")
 question = ("")
 answer = ""
 random_number = random.randint(1, 9)
 
+#checking to see if name is empty
+
 if len(name) == 0:
       print(question)
       print("Outlook not so good!")
+      
+#if/elif statements for magic ball
+
 
 elif len(question) == 0:
       random_number = 0
@@ -33,6 +39,7 @@ elif random_number ==9:
 else:
     print("error")
 
+#length checking and response
 
 if len(name) >= 1 and len(question) >=1:
   print(name, "Asks", question,)
