f9b25d7f776b57b479f5fd7557d242f8334a3204
inclinegod <143661601+inclinegod@users.noreply.github.com>
2023-09-05 20:39:28 +0100

    Update shippingprogram.py
    Updated the code ( finished)

--- shippingprogram.py
+++ shippingprogram.py
@@ -1,48 +1,52 @@
-
-
 weight = 0
-ground_cost = 0
 drone_weight = 0
-drone_cost = 0
+## define vars
 
 
-
-#ground shipping
-
-if weight == 0:
-  print("no weight inputted")
-elif weight <= 2:
-  ground_cost = weight * 1.50 + 20
-elif weight >= 2 and weight <= 6:
-  ground_cost = weight * 3.00 + 20
-elif weight >= 6 and weight <= 10:
-  ground_cost = weight * 4.00 + 20
+#selection from user
+ground = int(input("input type of shipping.\n 1 for ground\n 2 for drone."))
+if ground == 2:
+  drone = 1
+  ground = 0
+elif ground == 1:
+  ground = 1
 else:
-  if weight > 10:
-    ground_cost = weight * 4.75 + 20
+    ground = int(input("input type of shipping. \n 1 for ground  \n 2 for drone.    "))
 
-if weight != 0:
-  print("\n To ship that parcel via ground shipping, it will cost : $",ground_cost)
 
-  ground_premium = 125
-  print("\n ground shipping premium costs : $",ground_premium )
-
-  ##drone shipping
-
-  if drone_weight == 0:
-    print("\nno weight inputted")
-    drone_cost = 0
-elif drone_weight <= 2:
-  drone_cost = drone_weight * 4.50
-elif drone_weight >= 2 and drone_weight <= 6:
-  drone_cost = drone_weight * 9.00
-elif drone_weight >= 6 and drone_weight <= 10:
-  drone_cost = drone_weight * 12.00
+#ground shipping calculation
+if ground == 1:
+  weight = int(input("input weight of parcel : "))
+  if weight == 0:
+    print("no weight inputted ")
+  elif weight <= 2:
+    ground_cost = weight * 1.50 + 20
+  elif weight >= 2 and weight <= 6:
+    ground_cost = weight * 3.00 + 20
+  elif weight >= 6 and weight <= 10:
+    ground_cost = weight * 4.00 + 20
+  elif weight > 10:
+      ground_cost = weight * 4.75 + 20
+  if weight != 0:
+    print("\n To ship that parcel via ground shipping, it will cost : $ ",ground_cost)
+    ground_premium = 125
+    print("\n ground shipping premium costs : $",ground_premium )
 else:
-  if drone_weight >= 10:
-    drone_cost = drone_weight * 14.25
+  if drone == 1:  #drone shipping calculation
+    drone_weight = int(input("input drone weight "))
+    if drone_weight == 0:
+      print("\nno weight inputted ")
+    elif drone_weight <= 2:
+      drone_cost = drone_weight * 4.50
+    elif drone_weight >= 2 and drone_weight <= 6:
+      drone_cost = drone_weight * 9.00
+    elif drone_weight >= 6 and drone_weight <= 10:
+      drone_cost = drone_weight * 12.00
+    elif drone_weight >=10:
+      drone_cost = drone_weight * 14.25
+    else:
+        drone_weight = int(input("input drone weight "))
 
+#prints final statement
 if drone_weight != 0:
-  print("\n To ship that parcel via drone shipping, it will cost : $",drone_cost)
-
-
+  print("\n To ship that parcel via drone shipping, it will cost : $ ",drone_cost)
